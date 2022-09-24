5c489ca180189e58493ff667e7405890ed5d3027
gaweshab <89149747+gaweshab@users.noreply.github.com>
2022-09-24 18:10:57 +0530

    initial commit
    initial commit

---
+++ aboutus.php
@@ -0,0 +1,214 @@
+<h1>About Us</h1>
+<div class="intro">
+  <div class="container">
+    <div class="row row-eq-height">
+
+      <!-- Intro Content -->
+      <div class="col-lg-6">
+        <div class="intro_content">
+          <div class="section_title">
+            <div>Hello</div>
+            <h1>Amazing Hotel in front of the Sea</h1>
+          </div>
+          <div class="intro_text">
+            <p>Spread across the length and breadth of our tropical island, our homes of Sri Lankan hospitality invite you to indulge in our signature brand of luxury, brought to life with the strength of our well-rooted family values. No matter where you may find yourself in Sri Lanka, our doors will always be open.</p>
+          </div>
+          <!--  <div class="button intro_button"><a href="#">read more</a></div> -->
+        </div>
+      </div>
+
+      <!-- Intro Image -->
+      <div class="col-lg-6">
+        <div class="intro_image">
+          <div class="background_image" style="background-image:url(images/intro.jpg)"></div>
+          <img src="images/intro.jpg" alt="">
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<!-- Offering -->
+
+<div class="offering">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="section_title text-center">
+          <div>Resort</div>
+          <h1>What we offer</h1>
+        </div>
+      </div>
+    </div>
+    <div class="row offering_row">
+
+      <!-- Offer Item -->
+      <div class="col-xl-4 col-md-6">
+        <div class="offer">
+          <div class="offer_image"><img src="images/offer_1.jpg" alt=""></div>
+          <div class="offer_content text-center">
+            <div class="offer_title">
+              <h3>Outdoor Pool</h3>
+            </div>
+            <div class="offer_text">
+              <p>Immerse yourself in the relaxed atmosphere of our outdoor pool. Feel all your tiredness wash away and a sense of calm settle over your senses as you swim a few laps and take in the natural beauty.</p>
+            </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+          </div>
+        </div>
+      </div>
+
+      <!-- Offer Item -->
+      <div class="col-xl-4 col-md-6">
+        <div class="offer">
+          <div class="offer_image"><img src="images/offer_2.jpg" alt=""></div>
+          <div class="offer_content text-center">
+            <div class="offer_title">
+              <h3>Indoor Pool</h3>
+            </div>
+            <div class="offer_text">
+              <p>Perfect for any mood, our spacious indoor pool is the best place to take a dip after a day of sightseeing or to relax with friends and family.</p>
+            </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+          </div>
+        </div>
+      </div>
+
+      <!-- Offer Item -->
+      <div class="col-xl-4 col-md-6">
+        <div class="offer">
+          <div class="offer_image"><img src="images/offer_3.jpg" alt=""></div>
+          <div class="offer_content text-center">
+            <div class="offer_title">
+              <h3>Spa Zone</h3>
+            </div>
+            <div class="offer_text">
+              <p>It is the highest level of relaxation. We offer our guests a rich SPA offer with relaxing, beautifying, rejuvenating facial and body treatments and a wide range of massages.</p>
+            </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+          </div>
+        </div>
+      </div>
+
+      <!-- Offer Item -->
+      <div class="col-xl-4 col-md-6">
+        <div class="offer">
+          <div class="offer_image"><img src="images/offer_4.jpg" alt=""></div>
+          <div class="offer_content text-center">
+            <div class="offer_title">
+              <h3>Sports Area</h3>
+            </div>
+            <div class="offer_text">
+              <p>Get your adrenaline pumping and spend a fun time engaging in the various sports with your loved ones. Make unforgettable memories with them.</p>
+            </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+          </div>
+        </div>
+      </div>
+
+      <!-- Offer Item -->
+      <div class="col-xl-4 col-md-6">
+        <div class="offer">
+          <div class="offer_image"><img src="images/offer_5.jpg" alt=""></div>
+          <div class="offer_content text-center">
+            <div class="offer_title">
+              <h3>Restaurant</h3>
+            </div>
+            <div class="offer_text">
+              <p>Get your apetite up with all your preference. After a refreshing bath, exhausting journeys, or simply treat your taste buds with the delicious food we have to offer.</p>
+            </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+          </div>
+        </div>
+      </div>
+
+      <!-- Offer Item -->
+      <div class="col-xl-4 col-md-6">
+        <div class="offer">
+          <div class="offer_image"><img src="images/offer_6.jpg" alt=""></div>
+          <div class="offer_content text-center">
+            <div class="offer_title">
+              <h3>Skybar</h3>
+            </div>
+            <div class="offer_text">
+              <p>Sip a remarkable cocktail and nibble on a delicious selection of tapas while the gentle breeze through the open window adds a touch of coolness to the sultry night.</p>
+            </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<!-- Discover -->
+
+<div class="discover">
+
+  <!-- Discover Content -->
+  <div class="discover_content">
+    <div class="container">
+      <div class="row">
+
+        <!-- Discover Content -->
+        <div class="col-xl-5 col-lg-6">
+          <div class="section_title">
+            <div>Hotel</div>
+            <h1>Discover Royal Hotel</h1>
+          </div>
+          <div class="discover_highlight">
+            <p>As we understand how important connectivity, efficiency and comfort are to our business travellers, we have designed a set of benefits to help your company’s associates achieve their business goals wherever travel may take them.</p>
+          </div>
+          <div class="discover_text">
+            <p>Our Asian heritage places us in a unique position to understand and meet the special needs and expectations of multi generational families. With Fam.ily, we hope to serve them better by providing opportunities to create memorable experiences as they explore, bond, and grow as a family. Beyond hospitality, Fam.ily also works with experts to support our customers on their parenting journeys.</p>
+          </div>
+          <div class="button discover_button"><a href="#">discover</a></div>
+        </div>
+
+        <!-- 
+        <div class="col-lg-6 offset-xl-1">
+          <div class="milestones d-flex flex-row align-items-start justify-content-start flex-wrap">
+
+            
+            <div class="milestone">
+              <div class="milestone_counter" data-end-value="75">0</div>
+              <div class="milestone_text">Deluxe Rooms</div>
+            </div>
+
+            
+            <div class="milestone">
+              <div class="milestone_counter" data-end-value="110">0</div>
+              <div class="milestone_text">Years of Experience</div>
+            </div>
+
+            
+            <div class="milestone">
+              <div class="milestone_counter" data-end-value="31">0</div>
+              <div class="milestone_text">Awards Won</div>
+            </div>
+
+            
+            <div class="milestone">
+              <div class="milestone_counter" data-end-value="51" data-sign-after="k">0</div>
+              <div class="milestone_text">Happy Clients</div>
+            </div>
+
+          </div>
+        </div>
+          -->
+
+        <!-- Intro Image -->
+        <div class="col-lg-6">
+          <div class="intro_image">
+            <div class="background_image" style="background-image:url(images/111.jpg)"></div>
+            <img src="images/111.jpg" alt="">
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+</div>
